446fe3415a59cdea0ad05a0aaf202d8f9fb06c49
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-08 20:29:15 -0700

    Renaming Home to HomeScreen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from "./screens/Home";
+import HomeScreen from "./screens/HomeScreen";
 
 const Stack = createNativeStackNavigator();
 
