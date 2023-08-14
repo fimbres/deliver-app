eaf0afa3c5af2248c1c07d93e83ad36007449f00
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-13 21:17:45 -0700

    fixing code

--- utils/sanity.ts
+++ utils/sanity.ts
@@ -1,13 +1,15 @@
 import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
+import { Image } from './types';
 
 const client = createClient({
     projectId: 'minkpbjk',
     dataset: 'production',
     useCdn: true,
+    apiVersion: '2021-08-31',
 });
 
 const builder = imageUrlBuilder(client);
 
-export const urlFor = (src: string) => builder.image(src);
+export const urlFor = (src: Image) => builder.image(src);
 export default client;
