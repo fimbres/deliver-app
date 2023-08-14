d82f9afcf94eb0b3bfe3a2cc42e3d8c872c003b7
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-13 17:43:48 -0700

    hidding header

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
