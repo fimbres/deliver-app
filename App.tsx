234695d9aa8e6b26305b64caff76d55e82125c2e
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-14 01:03:46 -0700

    Adding navigation in App.tsx

--- App.tsx
+++ App.tsx
@@ -2,14 +2,22 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from "./screens/HomeScreen";
+import RestaurantScreen from "./screens/RestaurantScreen";
+import { Restaurant } from "./utils/types";
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  HomeScreen: undefined;
+  RestaurantScreen: { restaurant: Restaurant };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="RestaurantScreen" component={RestaurantScreen} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
