b7a4e9fdc6c3370233c941f4a3390246cf0284c1
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-13 21:18:07 -0700

    changing width

--- components/CategoryCard.tsx
+++ components/CategoryCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, ImageSourcePropType, Image, TouchableOpacity } from 'react-native'
+import { Text, ImageSourcePropType, Image, TouchableOpacity } from 'react-native'
 
 interface CategoryCardProps {
     text: string;
@@ -10,7 +10,7 @@ interface CategoryCardProps {
 const CategoryCard: React.FC<CategoryCardProps> = ({ text, image, onPress }) => {
   return (
     <TouchableOpacity onPressIn={onPress} className='relative mr-2'>
-      <Image source={image} className='h-20 w-20 rounded'/>
+      <Image source={image} className='h-20 w-44 rounded'/>
       <Text className='absolute bottom-1 left-1 text-white font-bold'>{text}</Text>
     </TouchableOpacity>
   )
