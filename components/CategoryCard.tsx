9b143cda078edc8188b27a93488b2a6c30787438
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-13 17:44:22 -0700

    new CategoryCard component

---
+++ components/CategoryCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View, Text, ImageSourcePropType, Image, TouchableOpacity } from 'react-native'
+
+interface CategoryCardProps {
+    text: string;
+    image: ImageSourcePropType;
+    onPress?: () => void;
+}
+
+const CategoryCard: React.FC<CategoryCardProps> = ({ text, image, onPress }) => {
+  return (
+    <TouchableOpacity onPressIn={onPress} className='relative mr-2'>
+      <Image source={image} className='h-20 w-20 rounded'/>
+      <Text className='absolute bottom-1 left-1 text-white font-bold'>{text}</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default CategoryCard
