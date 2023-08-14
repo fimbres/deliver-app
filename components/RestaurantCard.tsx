b66f9505970815e39dbdda53d3107b6f7ef0cf7e
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-13 17:44:33 -0700

    new RestaurantCard component

---
+++ components/RestaurantCard.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { StarIcon, MapPinIcon } from 'react-native-heroicons/solid';
+
+interface RestaurantCardProps {
+    id: string;
+    imageUrl: string;
+    title: string;
+    rating: number;
+    genre: string;
+    address: string;
+    shortDescription: string;
+    dishes: number;
+    long: number;
+    lat: number;
+}
+
+const RestaurantCard: React.FC<RestaurantCardProps> = ({
+    id,
+    imageUrl,
+    title,
+    rating,
+    genre,
+    address,
+    shortDescription,
+    dishes,
+    long,
+    lat,
+}) => {
+  return (
+    <TouchableOpacity className='bg-white mr-3 shadow'>
+      <Image source={{ uri: imageUrl }} className='h-36 w-56 rounded-sm'/>
+      <View className='px-3 pb-4'>
+        <Text className='font-bold text-lg pt-2'>{title}</Text>
+        <View className='flex-row items-center space-x-1'>
+            <StarIcon color='green' opacity={0.5} size={22} />
+            <Text className='text-xs text-gray-400'>
+                <Text className='text-green-500'>{rating}</Text> - {genre}
+            </Text>
+        </View>
+        <View className='flex-row items-center space-x-1'>
+            <MapPinIcon color='gray' opacity={0.5} size={22} />
+            <Text className='text-xs text-gray-500'>Nearby: {address}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+export default RestaurantCard
